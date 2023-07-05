aa64820be81f6b68d36ff4023650eed92442763e
Patrick Stapleton <132307108+patrickpstapleton@users.noreply.github.com>
2023-07-05 12:10:18 -0400

    create function to compare computer vs player

--- javascript.js
+++ javascript.js
@@ -15,3 +15,41 @@ function getComputerChoice() {
       throw console.error("Error: Computer has made an invalid choice");
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelectionHead = playerSelection.substring(0,1).toUpperCase()
+  playerSelectionTail = playerSelection.substring(1).toLowerCase()
+  playerPick = playerSelectionHead + playerSelectionTail
+
+  if (playerPick == computerSelection) {
+    return `It's a tie! Both picked ${playerPick}`
+  }
+  else if (playerPick == ROCK) {
+    if (computerSelection == SCISSORS) {
+      return `Player wins! Rock beats scissors`
+    }
+    else {
+      return `Computer wins! Paper beats rock`
+    }
+  }
+  else if (playerPick == PAPER) {
+    if (computerSelection == SCISSORS) {
+      return `Computer wins! Scissors beats paper`
+    }
+    else {
+      return `Player wins! Paper beats rock`
+    }
+  }
+  else {
+    if (computerSelection == PAPER) {
+      return `Player wins! Scissors beats paper`
+    }
+    else {
+      return `Computer wins! Rock beats scissors`
+    }
+  }
+}
+ 
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
